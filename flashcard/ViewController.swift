a2249d29789e4a0c7b3c8e02017279a2a33ee35a
Tenzin Pema <tpema1@binghamton.edu>
2018-10-13 17:42:25 -0400

    Initial Commit

---
+++ flashcard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flashcard
+//
+//  Created by Tenzin Pema on 10/13/18.
+//  Copyright © 2018 Tenzin Pema. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
